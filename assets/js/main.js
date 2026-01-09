b60558e7ce38f0b6dc122153a34cbfa0b64cc955
pujo-arthomo <pujo.arthomo@gmail.com>
2026-01-09 10:15:44 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 // future JS:
 // - announcement loader
+// Load announcement text
+fetch('data/announcement.json')
+  .then(res => res.json())
+  .then(data => {
+    const el = document.getElementById('announcement-text');
+    if (el) el.textContent = data.text;
+  })
+  .catch(err => console.log('Announcement error:', err));
+
 // - carousel scroll
 // - chili animation
